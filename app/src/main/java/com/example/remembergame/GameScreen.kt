540ddb63f9b594c4cc10e046d939e80e1b1d527e
veydoge <veydoge@gmail.com>
2025-06-23 22:10:30 +0300

    Добавлено сообщение о победе

--- app/src/main/java/com/example/remembergame/GameScreen.kt
+++ app/src/main/java/com/example/remembergame/GameScreen.kt
@@ -13,11 +13,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.remembergame.MemoryCard
 import kotlinx.coroutines.delay
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 data class Card(
     val id: Int,
@@ -32,11 +35,19 @@ val cardList = (cardValues + cardValues).shuffled().mapIndexed { index, value ->
 }
 
 @OptIn(ExperimentalFoundationApi::class)
+@Preview()
 @Composable
 fun GameScreen() {
     var cards by remember { mutableStateOf(cardList) }
     var flippedCards by remember { mutableStateOf(listOf<Card>()) }
     var blockInput by remember { mutableStateOf(false) }
+    val isWin by remember(cards){
+        derivedStateOf { cards.all{it.isMatched} }
+    }
+
+
+
+
 
     // Сравнение карт при открытии двух
     LaunchedEffect(flippedCards) {
@@ -61,34 +72,42 @@ fun GameScreen() {
             flippedCards = emptyList()
             blockInput = false
         }
+
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFFAF8FF)),
-        contentAlignment = Alignment.Center
-    ) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(3),
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            horizontalArrangement = Arrangement.spacedBy(12.dp)
+    Column(modifier = Modifier.fillMaxSize()
+        .background(Color(0xFFFAF8FF)),){
+        Spacer(modifier = Modifier.height(LocalConfiguration.current.screenHeightDp.dp * 0.2f))
+        Text(text = if (isWin) "Вы победили" else "", fontSize = 28.sp, modifier = Modifier.align(Alignment.CenterHorizontally))
+
+        Box(
+            contentAlignment = Alignment.Center
         ) {
-            itemsIndexed(cards) { index, card ->
-                MemoryCard(
-                    card = card,
-                    onClick = {
-                        if (blockInput || card.isFlipped || card.isMatched || flippedCards.size == 2) return@MemoryCard
 
-                        val newCard = card.copy(isFlipped = true)
-                        val newCards = cards.toMutableList()
-                        newCards[index] = newCard
-                        cards = newCards
-                        flippedCards = flippedCards + newCard
-                    }
-                )
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                itemsIndexed(cards) { index, card ->
+                    MemoryCard(
+                        card = card,
+                        onClick = {
+                            if (blockInput || card.isFlipped || card.isMatched || flippedCards.size == 2) return@MemoryCard
+
+                            val newCard = card.copy(isFlipped = true)
+                            val newCards = cards.toMutableList()
+                            newCards[index] = newCard
+                            cards = newCards
+                            flippedCards = flippedCards + newCard
+                        }
+                    )
+                }
             }
+
         }
+
     }
+
 }
